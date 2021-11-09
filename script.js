8c11f74758b66695d13a46b28ec825899efee951
Prince-Thind <princethind799@gmail.com>
2021-11-09 14:31:14 +0530

    refactor code

--- script.js
+++ script.js
@@ -1,55 +1,29 @@
 const contentRoot = document.querySelector("#content");
 
-function createText() {
-  const lorem = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, ipsa. Deleniti corporis velit delectus distinctio earum voluptatum ullam numquam dolorum necessitatibus fuga, recusandae iste vitae sit architecto obcaecati maxime saepe.`;
-  const num = Math.trunc(Math.random() * 3 + 1);
-  return lorem.repeat(num);
-}
-function createPara() {
-  const para = document.createElement("p");
-  para.classList.add("section-para");
-  para.textContent = createText();
-  return para;
-}
-function randomParas() {
-  const random = Math.trunc(Math.random() * 4 + 1);
-  const arr = [];
-  for (let i = 0; i < random; i++) {
-    const para = createPara();
-    arr.push(para);
-  }
-  return arr;
-}
+main();
 
-function createListItem() {
-  const item = document.createElement("li");
-  item.textContent = createText();
-  return item;
-}
-function randomListItems() {
-  const random = Math.trunc(Math.random() * 3 + 1);
-  const arr = [];
-  for (let i = 0; i < random; i++) {
-    const item = createListItem();
-    arr.push(item);
+function main() {
+  contentRoot.innerHTML = "";
+  for (let i = 1; i <= 15; i++) {
+    const section = createSection(i);
+    contentRoot.appendChild(section);
   }
-  return arr;
 }
 
 function createSection(sectionNum) {
   const section = document.createElement("section");
-  section.classList.add('section');
-  section.setAttribute('id',`topic-${sectionNum}`);
+  section.classList.add("section");
+  section.setAttribute("id", `topic-${sectionNum}`);
 
   const header = document.createElement("header");
   section.appendChild(header);
 
   const h3 = document.createElement("h3");
   h3.classList.add("section-heading");
-  h3.textContent=`Topic ${sectionNum}`;
+  h3.textContent = `Topic ${sectionNum}`;
   header.appendChild(h3);
 
-  const paras =randomParas();
+  const paras = randomParas();
   for (let para of paras) {
     section.appendChild(para);
   }
@@ -70,11 +44,41 @@ function createSection(sectionNum) {
   return section;
 }
 
-function main() {
-  contentRoot.innerHTML = "";
-  for (let i = 1; i <= 15; i++) {
-    const section = createSection(i);
-    contentRoot.appendChild(section);
-  }
+function createText() {
+  const lorem = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, ipsa. Deleniti corporis velit delectus distinctio earum voluptatum ullam numquam dolorum necessitatibus fuga, recusandae iste vitae sit architecto obcaecati maxime saepe.`;
+  const num = Math.trunc(Math.random() * 3 + 1);
+  return lorem.repeat(num);
 }
-main();
+
+function randomParas() {
+  const random = Math.trunc(Math.random() * 4 + 1);
+  const arr = [];
+  for (let i = 0; i < random; i++) {
+    const para = createPara();
+    arr.push(para);
+  }
+  return arr;
+}
+
+function createPara() {
+  const para = document.createElement("p");
+  para.classList.add("section-para");
+  para.textContent = createText();
+  return para;
+}
+
+function randomListItems() {
+  const random = Math.trunc(Math.random() * 3 + 1);
+  const arr = [];
+  for (let i = 0; i < random; i++) {
+    const item = createListItem();
+    arr.push(item);
+  }
+  return arr;
+}
+
+function createListItem() {
+  const item = document.createElement("li");
+  item.textContent = createText();
+  return item;
+}
