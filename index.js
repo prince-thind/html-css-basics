ea2f87ab366189824619d0208e77e31bed5827e5
Prince Thind <Princethind799@gmail.com>
2021-11-29 01:10:48 +0530

    add my favorite punjabi quote

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const mainText="Let Them \nAssume"
+const mainText =
+  "ਕਾਲੀ ਰਾਤ ਦੀ ਸਿਆਹੀ ਉੱਤੇ\nਚੱਦ ਦੀ ਗਵਾਹੀ ਉੱਤੇ\nਸੋਚ ਦੇ ਕੋਰੇ ਕਾਗਜ਼ ਤੇ\nਇੱਕ ਅਖਰ ਮੈਂ ਬਣ ਜਾਵਾਂ\nਤੂੰ ਭੁੱਲੇ ਤਾ ਤੇਰੀ ਮਰਜ਼ੀ \nਮੈਂ ਭੁੱਲਾਂ ਤਾਂ ਮਰ ਜਾਵਾਂ";
 
-const target=document.querySelector('#target');
-target.textContent=mainText;
-target.setAttribute('data-neon',mainText);
+const target = document.querySelector("#target");
+target.textContent = mainText;
+target.setAttribute("data-neon", mainText);
