23d1c1cf53694463d274b098318c15bab09f058a
Prince Thind <Princethind799@gmail.com>
2022-02-16 20:48:29 +0530

    implement basic snow

--- index.js
+++ index.js
@@ -1,21 +1,69 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+class Particle {
+  constructor(positionX, positionY) {
+    this.x = positionX ?? Math.random() * window.innerWidth;
+    this.y = positionY ?? 0;
+    this.size = Math.random() * 10;
+    this.speedX = Math.random() - 0.5;
+    this.speedY = Math.random();
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.speedX = Math.random() - 0.5;
+    this.speedY = Math.random() * 5;
+    if (this.size > 0.5) this.size -= 0.01;
+  }
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.fill();
+  }
+}
+
+let particles = [];
+
 fitToScreen(canvas);
+init();
 animate();
 
 function animate() {
   ctx.fillStyle = "rgba(0,0,0,1)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  animateCircle();
+  animateParticles();
   requestAnimationFrame(animate);
 }
 
-function animateCircle(){
-    ctx.strokeStyle='white'
-    ctx.beginPath();
-    ctx.arc(75, 75, 50, 0, Math.PI * 2); // Outer circle
-    ctx.stroke();
+function animateParticles() {
+  for (const particle of particles) {
+    particle.draw();
+    particle.update();
+  }
+  refreshParticles();
+}
+
+function init() {
+  for (let i = 0; i < 100; i++) {
+    const particle = new Particle(
+      window.innerWidth * Math.random(),
+      window.innerHeight * Math.random()
+    );
+    particles.push(particle);
+  }
+}
+
+function refreshParticles() {
+  while (particles.length < 100) {
+    const particle = new Particle();
+    particles.push(particle);
+  }
+  particles = particles.filter((p) => p.y <= canvas.height - 20);
 }
 
 function fitToScreen(canvas) {
