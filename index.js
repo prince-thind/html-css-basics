2f28bf2a8cc7d36278e1e10d53a47813270e508b
Prince Thind <Princethind799@gmail.com>
2022-02-16 19:58:54 +0530

    draw basic circle

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 fitToScreen(canvas);
+animate();
+
+function animate() {
+  ctx.fillStyle = "rgba(0,0,0,1)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  animateCircle();
+  requestAnimationFrame(animate);
+}
+
+function animateCircle(){
+    ctx.strokeStyle='white'
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2); // Outer circle
+    ctx.stroke();
+}
 
 function fitToScreen(canvas) {
   canvas.height = window.innerHeight;
@@ -12,6 +27,3 @@ function fitToScreen(canvas) {
     canvas.width = window.innerWidth;
   });
 }
-
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 150, 100);
