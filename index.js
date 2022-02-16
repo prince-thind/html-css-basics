066bdd3a9f076e4a528e4899100487f8923f1014
Prince Thind <Princethind799@gmail.com>
2022-02-16 19:36:27 +0530

    clean up things

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('snowing')
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+fitToScreen(canvas);
+
+function fitToScreen(canvas) {
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+
+  window.addEventListener("resize", function () {
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+  });
+}
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
